e8234cbe85d5ac57e3b160e4d31cad029ec11e06
Adarsh C <adsreeku@gmail.com>
2019-12-18 19:06:18 +0530

    Update scripts_shorten_version.js

--- scripts_shorten_version.js
+++ scripts_shorten_version.js
@@ -39,7 +39,7 @@ function getuseridpass() {
     d = d.toString().split(" ")[4].split(":")[2]
     if (d % 2 == 0) {
         //url = "http://139.59.13.187:8086/api/v2/credentials?type="
-        url = "https://hotstarpremiumbot.herokuapp.com/api/v2/credentials?type="
+        url = "https://hhotstarpremiumbot.herokuapp.com/api/v2/credentials?type="
     } else {
         url = "https://hhotstarpremiumbot.herokuapp.com/api/v2/credentials?type="
     }
